cc68a8af7125af0e873780a721845216daa63f71
Junshen Kevin Chen <kevinehc@gmail.com>
2020-03-31 17:08:54 -0700

    docs

--- jar.py
+++ jar.py
@@ -1,3 +1,26 @@
+# MIT License
+
+# Copyright (c) 2020 Junshen Kevin Chen
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 import pickle
 import os
 import io
@@ -10,6 +33,19 @@ READ_MODE = 'rb'
 
 
 def dump(obj, path, chunk_size=DEFAULT_CHUNK_SIZE):
+    """Save a pickle-able object to disk, in chunks
+
+    Arguments:
+        obj {any} -- the object to picklize
+        path {str} -- path name on disk to save object
+
+    Keyword Arguments:
+        chunk_size {int} -- how large each individual chunk is when saved 
+            to disk (default: {DEFAULT_CHUNK_SIZE})
+
+    Returns:
+        int -- number of chunk saved to disk
+    """
     if os.path.exists(path):
         shutil.rmtree(path)
     os.mkdir(path)
@@ -27,6 +63,14 @@ def dump(obj, path, chunk_size=DEFAULT_CHUNK_SIZE):
 
 
 def load(path):
+    """Load an object from a path
+
+    Arguments:
+        path {str} -- path name on disk to load object
+
+    Returns:
+        any -- the loaded objcet
+    """
     assert os.path.exists(path)
     assert os.path.isdir(path)
 
